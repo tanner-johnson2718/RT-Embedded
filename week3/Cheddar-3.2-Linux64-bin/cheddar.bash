9d521cb373b1b4c05d0ee3162992c937d2b27bbf
tanner-johnson2718 <tanner.johnson2718@gmail.com>
2022-01-18 15:42:11 -0900

    ..

---
+++ week3/Cheddar-3.2-Linux64-bin/cheddar.bash
@@ -0,0 +1,22 @@
+
+
+export CHEDDAR_INSTALL_PATH=/home/tj/Desktop/RT-Embedded/week2/hw3/Cheddar-3.2-Linux64-bin/
+
+export PATH=$CHEDDAR_INSTALL_PATH:$PATH
+
+export LIBRARY_PATH=$CHEDDAR_INSTALL_PATH/lib/linux64/gtk-2.0:\
+$CHEDDAR_INSTALL_PATH/lib/linux64:\
+/usr/lib64:\
+/usr/lib/x86_64-linux-gnu:\
+/lib/x86_64-linux-gnu:\
+$LIBRARY_PATH
+
+export LD_LIBRARY_PATH=:$LIBRARY_PATH:$LD_LIBRARY_PATH
+
+
+
+
+
+
+
+
